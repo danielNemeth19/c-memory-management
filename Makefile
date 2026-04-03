764cef451fafe7dc1bf4478865b935b502bd1a20
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2026-04-04 00:41:13 +0200

    (feat) Array decays to pointer as func arg

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ build:
 	gcc main.c -o main.o
 
 test:
-	gcc exercise_1.c coord.c snek.c test.c -o test_suite && ./test_suite
+	gcc exercise_1.c coord.c snek.c test.c -o test_suite.o && ./test_suite.o
 
 build-test:
 	gcc exercise_1.c coord.c snek.c test.c -o test_suite.o
--- array.c
+++ array.c
@@ -1,4 +1,5 @@
 #include "coord.h"
+#include <signal.h>
 #include <stdio.h>
 
 /* Example memory layout */
@@ -23,8 +24,8 @@ int multibyte_arrays(void) {
            points[1].x, points[1].y, points[1].z);
 
     coordinate_t *ptr = points;
-    printf("ptr[1].x = %d , ptr[1].y = %d, ptr[1].z = %d\n",
-           (ptr + 1)->x, (ptr + 1)->y, (ptr + 1)->z);
+    printf("ptr[1].x = %d , ptr[1].y = %d, ptr[1].z = %d\n", (ptr + 1)->x,
+           (ptr + 1)->y, (ptr + 1)->z);
     return 0;
 }
 
@@ -34,15 +35,29 @@ int array_casting(void) {
         {7, 3, 2},
         {9, 6, 8},
     };
-    int *points_start = (int *) points;
+    int *points_start = (int *)points;
     for (int x = 0; x < 9; x++) {
         printf("x here is %d\n", points_start[x]);
     };
     return 0;
 }
 
+void core_utils_func(int core_utilization[]) {
+    printf("sizeof core_utilization in core_utils_func: %zu\n",
+           sizeof(core_utilization));
+}
+
+void passed_array_decays_to_pointer_test(void) {
+    int core_utilization[] = {34, 35, 456, 34, 67, 23, 87, 44};
+    int len = sizeof(core_utilization) / sizeof(core_utilization[0]);
+    printf("sizeof core_utilization in main %zu\n", sizeof(core_utilization));
+    printf("len of core_utilization: %d\n", len);
+    core_utils_func(core_utilization);
+}
+
 int main(void) {
     multibyte_arrays();
     array_casting();
+    passed_array_decays_to_pointer_test();
     return 0;
 }
