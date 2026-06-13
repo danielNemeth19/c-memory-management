62d87952a75d6b3f2d1ab34cbd3182f6d72e9531
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2026-06-13 23:54:40 +0200

    (feat) Adds vector object

--- 09_objects/snekobject.c
+++ 09_objects/snekobject.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+snek_object_t *new_snek_vector3(snek_object_t *x, snek_object_t *y, snek_object_t *z) {
+    if ((x == NULL ) || (y == NULL) || (z == NULL)) {
+        return NULL;
+    }
+    snek_object_t *s_obj = malloc(sizeof(snek_object_t));
+    if (s_obj == NULL) {
+        return NULL;
+    }
+
+    s_obj->kind = VECTOR3;
+    s_obj->data.v_vector3.x = x;
+    s_obj->data.v_vector3.y = y;
+    s_obj->data.v_vector3.z = z;
+    return s_obj;
+}
+
 snek_object_t *new_snek_string(char *value) {
     snek_object_t *s_obj = malloc(sizeof(snek_object_t));
     if (s_obj == NULL) {
--- 09_objects/snekobject_test.c
+++ 09_objects/snekobject_test.c
@@ -1,7 +1,8 @@
 #include "snekobject.h"
 #include <assert.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
 static int int_equal(int a, int b) { return (a - b) == 0; };
 
@@ -9,6 +10,11 @@ static int float_equal(float a, float b) {
     const float epsilon = 1e-6f;
     return (a - b < epsilon) && (b - a < epsilon);
 }
+
+static int string_equal(const char *a, const char *b) {
+    return strcmp(a, b) == 0;
+};
+
 static int ptr_not_null(void *ptr, char *message) {
     bool verdict = ptr != NULL;
     if (!verdict) {
@@ -91,8 +97,42 @@ void test_snek_string_copied(void) {
     snek_object_t *obj = new_snek_string(input);
 
     assert(int_equal(obj->kind, STRING));
-    assert(ptr_not_equal(input, obj->data.v_string, "Pointers should be different"));
-    assert(input == obj->data.v_string);
+    assert(ptr_not_equal(input, obj->data.v_string,
+                         "Pointers should be different"));
+    assert(string_equal(input, obj->data.v_string));
+
+    free(obj->data.v_string);
+    free(obj);
+}
+
+void test_snek_vector3_returns_null(void) {
+    snek_object_t *obj1 = new_snek_float(10.1);
+    snek_object_t *obj2 = new_snek_integer(10);
+
+    snek_object_t *s_obj = new_snek_vector3(obj1, obj2, NULL);
+    assert(s_obj == NULL);
+}
+
+void test_snek_vector3_multiple_objects(void) {
+    snek_object_t *obj1 = new_snek_float(10.1);
+    snek_object_t *obj2 = new_snek_integer(10);
+    snek_object_t *obj3 = new_snek_string("Blaa");
+
+    snek_object_t *vec = new_snek_vector3(obj1, obj2, obj3);
+    assert(ptr_not_null(vec, "Should allocate pointer"));
+
+    assert(obj1 == vec->data.v_vector3.x);
+    assert(obj2 == vec->data.v_vector3.y);
+    assert(obj3 == vec->data.v_vector3.z);
+
+    assert(float_equal(vec->data.v_vector3.x->data.v_float, 10.1));
+    assert(int_equal(vec->data.v_vector3.y->data.v_int, 10));
+    assert(string_equal(vec->data.v_vector3.z->data.v_string, "Blaa"));
+
+    free(obj1);
+    free(obj2);
+    free(obj3);
+    free(vec);
 }
 
 int main(void) {
@@ -105,6 +145,8 @@ int main(void) {
     test_snek_float_zero();
     test_snek_float_negative();
     test_snek_string_copied();
+    test_snek_vector3_returns_null();
+    test_snek_vector3_multiple_objects();
     printf("All tests passed.\n");
     return 0;
 }
