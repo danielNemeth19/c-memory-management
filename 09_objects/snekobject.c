e40a348070a6685cf94b8232c5660afbb37585d3
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2026-06-25 23:15:12 +0200

    (chore) Eliminates nested swithes for snek_add

--- 09_objects/snekobject.c
+++ 09_objects/snekobject.c
@@ -3,20 +3,103 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void _free_vectors(snek_object_t *vx, snek_object_t *vy,
-                          snek_object_t *vz) {
-    if (vx != NULL && vx->kind == STRING) {
-        free(vx->data.v_string);
+static snek_object_t *_snek_add_int_to_number(int a_int, snek_object_t *b) {
+    switch (b->kind) {
+    case INTEGER: {
+        int sum = a_int + b->data.v_int;
+        return new_snek_integer(sum);
     }
-    if (vy != NULL && vy->kind == STRING) {
-        free(vy->data.v_string);
+    case FLOAT: {
+        float sum = (float)a_int + b->data.v_float;
+        return new_snek_float(sum);
     }
-    if (vz != NULL && vz->kind == STRING) {
-        free(vz->data.v_string);
+    default:
+        return NULL;
     }
-    free(vx);
-    free(vy);
-    free(vz);
+}
+
+static snek_object_t *_snek_add_float_to_number(float a_flt, snek_object_t *b) {
+    switch (b->kind) {
+    case INTEGER: {
+        float sum = a_flt + (float)b->data.v_int;
+        return new_snek_float(sum);
+    }
+    case FLOAT: {
+        float sum = a_flt + b->data.v_float;
+        return new_snek_float(sum);
+    }
+    default:
+        return NULL;
+    }
+}
+
+static snek_object_t *_snek_add_str(snek_object_t *a, snek_object_t *b) {
+    switch (b->kind) {
+    case STRING: {
+        size_t t_len = strlen(a->data.v_string) + strlen(b->data.v_string) + 1;
+        char *interim = calloc(t_len, sizeof(char));
+        if (interim == NULL) {
+            return NULL;
+        }
+        strcat(interim, a->data.v_string);
+        strcat(interim, b->data.v_string);
+        snek_object_t *new_obj = new_snek_string(interim);
+        free(interim);
+        return new_obj;
+    }
+    default:
+        return NULL;
+    }
+}
+
+static snek_object_t *_snek_add_vector3(snek_object_t *a, snek_object_t *b) {
+    switch (b->kind) {
+    case VECTOR3: {
+        snek_object_t *vx = snek_add(a->data.v_vector3.x, b->data.v_vector3.x);
+        snek_object_t *vy = snek_add(a->data.v_vector3.y, b->data.v_vector3.y);
+        snek_object_t *vz = snek_add(a->data.v_vector3.z, b->data.v_vector3.z);
+        if (vx == NULL || vy == NULL || vz == NULL) {
+            free(vx);
+            free(vy);
+            free(vz);
+            return NULL;
+        }
+        snek_object_t *new_obj = new_snek_vector3(vx, vy, vz);
+        if (new_obj == NULL) {
+            free(vx);
+            free(vy);
+            free(vz);
+            return NULL;
+        }
+        return new_obj;
+    }
+    default:
+        return NULL;
+    }
+}
+
+static snek_object_t *_snek_add_array(snek_object_t *a, snek_object_t *b) {
+    switch (b->kind) {
+    case ARRAY: {
+        size_t len = a->data.v_array.size + b->data.v_array.size;
+        snek_object_t *new_obj_array = new_snek_array(len);
+        if (new_obj_array == NULL) {
+            return NULL;
+        }
+        for (size_t i = 0; i < a->data.v_array.size; i++) {
+            snek_object_t *item = snek_array_get(a, i);
+            snek_array_set(new_obj_array, i, item);
+        }
+        size_t b_index = a->data.v_array.size;
+        for (size_t y = 0; y < b->data.v_array.size; y++) {
+            snek_object_t *item = snek_array_get(b, y);
+            snek_array_set(new_obj_array, b_index + y, item);
+        }
+        return new_obj_array;
+    }
+    default:
+        return NULL;
+    }
 }
 
 snek_object_t *snek_add(snek_object_t *a, snek_object_t *b) {
@@ -25,94 +108,15 @@ snek_object_t *snek_add(snek_object_t *a, snek_object_t *b) {
     }
     switch (a->kind) {
     case INTEGER:
-        switch (b->kind) {
-        case INTEGER: {
-            int sum = a->data.v_int + b->data.v_int;
-            return new_snek_integer(sum);
-        }
-        case FLOAT: {
-            float sum = a->data.v_int + b->data.v_float;
-            return new_snek_float(sum);
-        }
-        default:
-            return NULL;
-        }
+        return _snek_add_int_to_number(a->data.v_int, b);
     case FLOAT:
-        switch (b->kind) {
-        case INTEGER: {
-            float sum = a->data.v_float + (float)b->data.v_int;
-            return new_snek_float(sum);
-        }
-        case FLOAT: {
-            float sum = a->data.v_float + b->data.v_float;
-            return new_snek_float(sum);
-        }
-        default:
-            return NULL;
-        }
+        return _snek_add_float_to_number(a->data.v_float, b);
     case STRING:
-        switch (b->kind) {
-        case STRING: {
-            size_t t_len =
-                strlen(a->data.v_string) + strlen(b->data.v_string) + 1;
-            char *interim = calloc(t_len, sizeof(char));
-            if (interim == NULL) {
-                return NULL;
-            }
-            strcat(interim, a->data.v_string);
-            strcat(interim, b->data.v_string);
-            snek_object_t *new_obj = new_snek_string(interim);
-            free(interim);
-            return new_obj;
-        }
-        default:
-            return NULL;
-        }
+        return _snek_add_str(a, b);
     case VECTOR3:
-        switch (b->kind) {
-        case VECTOR3: {
-            snek_object_t *vx =
-                snek_add(a->data.v_vector3.x, b->data.v_vector3.x);
-            snek_object_t *vy =
-                snek_add(a->data.v_vector3.y, b->data.v_vector3.y);
-            snek_object_t *vz =
-                snek_add(a->data.v_vector3.z, b->data.v_vector3.z);
-            if (vx == NULL || vy == NULL || vz == NULL) {
-                _free_vectors(vx, vy, vz);
-                return NULL;
-            }
-            snek_object_t *new_obj = new_snek_vector3(vx, vy, vz);
-            if (new_obj == NULL) {
-                _free_vectors(vx, vy, vz);
-                return NULL;
-            }
-            return new_obj;
-        }
-        default:
-            return NULL;
-        }
+        return _snek_add_vector3(a, b);
     case ARRAY:
-        switch (b->kind) {
-        case ARRAY: {
-            size_t len = a->data.v_array.size + b->data.v_array.size;
-            snek_object_t *new_obj_array = new_snek_array(len);
-            if (new_obj_array == NULL) {
-                return NULL;
-            }
-            for (size_t i = 0; i < a->data.v_array.size; i++) {
-                snek_object_t *curr_arr = snek_array_get(a, i);
-                snek_array_set(new_obj_array, i, curr_arr);
-            }
-            size_t b_index = a->data.v_array.size;
-            for (size_t y = 0; y < b->data.v_array.size; y++) {
-                snek_object_t *curr_arr = snek_array_get(b, y);
-                snek_array_set(new_obj_array, b_index + y, curr_arr);
-            }
-            return new_obj_array;
-        }
-        default:
-            return NULL;
-        }
+        return _snek_add_array(a, b);
     }
     return NULL;
 }
@@ -170,12 +174,16 @@ snek_object_t *snek_add_old(snek_object_t *a, snek_object_t *b) {
         snek_object_t *vy = snek_add(a->data.v_vector3.y, b->data.v_vector3.y);
         snek_object_t *vz = snek_add(a->data.v_vector3.z, b->data.v_vector3.z);
         if (vx == NULL || vy == NULL || vz == NULL) {
-            _free_vectors(vx, vy, vz);
+            free(vx);
+            free(vy);
+            free(vz);
             return NULL;
         }
         snek_object_t *new_obj = new_snek_vector3(vx, vy, vz);
         if (new_obj == NULL) {
-            _free_vectors(vx, vy, vz);
+            free(vx);
+            free(vy);
+            free(vz);
             return NULL;
         }
         return new_obj;
